bcac256284a719ee5cbfc5f7b337d00952f059f0
Le Si Bich <moonlight8978@gmail.com>
2017-04-13 10:33:19 +0700

    update test

--- test/test.cpp
+++ test/test.cpp
@@ -14,20 +14,20 @@ struct Time {
 };
 
 struct Employee {
-	char 	manv[4];		// ma nhan vien
+	char 	manv[4];	    // ma nhan vien
 	char 	ho[30];
 	char 	ten[10];
 	char	congty[30];
 	char	chucvu[30];
 	Date	sinhnhat;
-	char 	que[20];		// que quan
+	char 	que[20];	    // que quan
 	char	diachi[50];
 	char	email[50];
-	int 	sdt;		// so dien thoai
-	Date	ngaybd;		// ngay bat dau
-	Date*   ngaylv;		// ngay lam viec
-	Time*	gioden;		// gio den^'
-	Time* 	giove;		// gio ve^`
+	char 	sdt[50];	    // so dien thoai
+	Date	ngaybd;		    // ngay bat dau
+	Date*   ngaylv;		    // ngay lam viec
+	Time*	gioden;		    // gio den^'
+	Time* 	giove;		    // gio ve^`
 };
 
 Employee* createEmployee() {
@@ -41,6 +41,7 @@ Employee* createEmployee() {
 void read(FILE* file) {
 	Employee* emp = createEmployee();
 	char* line = new char[100];
+	int i;
 	fgets(emp->manv, 100, file);
 	fgets(emp->ho, 100, file);
 	fgets(emp->ten, 100, file);
@@ -48,25 +49,43 @@ void read(FILE* file) {
 	fgets(emp->chucvu, 100, file);
 	fgets(line, 100, file);
 	sscanf(
-		line, 
-		"%d/%d/%d", 
+		line, "%d/%d/%d", 
 		&emp->sinhnhat.ngay, &emp->sinhnhat.thang, &emp->sinhnhat.nam
 	);
 	fgets(emp->que, 100, file);
 	fgets(emp->diachi, 100, file);
 	fgets(emp->email, 100, file);
-	//fgets(emp->sdt, 100, file);
-	//sscanf(line, "%d/%d,%d/%d/%d,%d/%d", emp->);
-	cout 	<< emp->sinhnhat.ngay 
-			<< emp->sinhnhat.thang 
-			<< emp->sinhnhat.nam;
+	fgets(emp->sdt, 100, file);
+	fgets(line, 100, file);
+	sscanf(
+		line, "%d/%d/%d", 
+		&emp->ngaybd.ngay, &emp->ngaybd.thang, &emp->ngaybd.nam
+	);
+	for (i = 0; i < 7; i += 1) {
+	    fgets(line, 100, file);
+	    sscanf(
+		    line, "%d/%d/%d,%d:%d,%d:%d", 
+		    &emp->ngaylv[i].ngay, &emp->ngaylv[i].thang, &emp->ngaylv[i].nam,
+		    &emp->gioden[i].gio, &emp->gioden[i].phut,
+		    &emp->giove[i].gio, &emp->giove[i].phut
+        );
+    }
+	cout 	<< emp->ngaylv[0].ngay   << "/"
+			<< emp->ngaylv[0].thang  << "/"
+			<< emp->ngaylv[0].nam    << " "
+			<< emp->gioden[0].gio    << ":"
+			<< emp->gioden[0].phut   << " "
+			<< emp->giove[0].gio    << ":"
+			<< emp->giove[0].phut   << " "
+			<< emp->sdt;
 }
 
 int main() {
 	const char *filePath = "E:/workspace/Cxxxxx/test-input/input.txt";
 	FILE *file;
-
-	file = fopen(filePath, "r");
+    file = fopen(filePath, "r");
+    
 	read(file);
+	
 	return 0;
 }
